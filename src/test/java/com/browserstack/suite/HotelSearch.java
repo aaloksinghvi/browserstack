4588e8df0b1792ac982a15fa24be48add6759cce
A.Singhvi <A.Singhvi>
2017-01-09 16:04:29 -0800

    Fourth Browser Stack Commit

--- src/test/java/com/browserstack/suite/HotelSearch.java
+++ src/test/java/com/browserstack/suite/HotelSearch.java
@@ -1,8 +1,6 @@
 package com.browserstack.suite;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,7 +9,7 @@ import com.browserstack.BrowserStackTestNGTest;
 public class HotelSearch extends BrowserStackTestNGTest {
 
     @Test
-    public void hotelSearch() throws Exception {
+    public void hotelSearch1() throws Exception {
        
     	driver.get("https://www.delta.com/merch/searchHotelAction.action");
        
@@ -21,16 +19,10 @@ public class HotelSearch extends BrowserStackTestNGTest {
     	driver.findElement(By.id("hotelCheckInDate")).sendKeys("01/24/2017");
     	driver.findElement(By.id("hotelCheckOutDate"));
     	driver.findElement(By.id("hotelCheckOutDate")).sendKeys("01/30/2017");
+  	
+    	driver.findElement(By.id("btnSubmit")).click();
     	
-    	/* USING DEFUALT AS 1
-    	 * driver.findElement(By.id("hotelNumberOfRooms-required"));
-    	driver.findElement(By.id("hotelNumberOfRooms-required")).sendKeys("2");
-    	
-    	driver.findElement(By.id("hotelNumberOfRooms-required"));
-    	driver.findElement(By.id("hotelNumberOfRooms-required")).sendKeys("2");
-    	*
-    	**/
 
-        Assert.assertEquals("Hotel Search Results : Delta Air Lines", driver.getTitle());
+        Assert.assertEquals("Hotel Search : Delta Air Lines", driver.getTitle());
     }
 }
